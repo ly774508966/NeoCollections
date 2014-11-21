8c211fd923bec379323338f37f48c74433bf7ab9
Jonas Thiel <jonas@thiel.io>
2014-11-21 11:21:49 +0100

    Remove datetime from AssemblyVersion

--- Assets/NeoCollections/AssemblyInfo.cs
+++ Assets/NeoCollections/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.20141121b")]
-[assembly: AssemblyFileVersion("1.0.1.20141121b")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
